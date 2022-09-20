b76212b0f4bc727188c391653db6b7ff07ee1f14
VISHAL KUMAR - Boolean Pirate <vishalkrnda@gmail.com>
2022-09-20 20:47:22 +0530

    Completed

--- src/com/services/bank/SocialBank.java
+++ src/com/services/bank/SocialBank.java
@@ -1,5 +1,8 @@
 package com.services.bank;
 
-public class SocialBank extends Hdfc{
+public class SocialBank extends HdfcBank{
+	//Multiple inheritance is not possible in java
+	//So public (class SocialBank extends HdfcBank,GenericBank {) will give an error
+	//Multiple inheritance in java is achived through interface
 
 }
